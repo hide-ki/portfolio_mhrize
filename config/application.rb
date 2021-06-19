fded2498b1c544d0070b1ffb000f84b99f5af8f2
hide-ki <okashiokashi@icloud.com>
2021-06-20 08:54:05 +0900

    ADD: タイムゾーンを東京に設定

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,11 @@ module PortfolioMhrize
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # タイムゾーンの設定
+    # アプリケーションの時刻の設定
+    config.time_zone = 'Tokyo'
+    # DBに記録されている時刻をどのタイムゾーンで読み込むか
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
