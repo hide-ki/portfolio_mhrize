1d8871bf26add835deec4bd8d72ea83367621c43
hide-ki <okashiokashi@icloud.com>
2021-06-25 11:33:35 +0900

    ADD: 重ね着装備の表示、投稿機能を追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,4 +14,46 @@ class Post < ApplicationRecord
   validates :image, presence: true
 
   enum character_gender: { gentlemen: 0, lady: 1 }
+
+
+  # 一括してメソッドを作りたい(modelの肥大化を防止)
+  def head_armor_name
+    if self.head_armor.nil?
+      'なし'
+    else
+      head_armor.name
+    end
+  end
+
+  def body_armor_name
+    if self.body_armor.nil?
+      'なし'
+    else
+      body_armor.name
+    end
+  end
+
+  def arm_armor_name
+    if self.arm_armor.nil?
+      'なし'
+    else
+      arm_armor.name
+    end
+  end
+
+  def waist_armor_name
+    if waist_armor.nil?
+      'なし'
+    else
+      waist_armor.name
+    end
+  end
+
+  def foot_armor_name
+    if foot_armor.nil?
+      'なし'
+    else
+      foot_armor.name
+    end
+  end
 end
