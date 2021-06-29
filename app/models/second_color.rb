02561a7b319508850a49b164292c6a8c264479e9
hide-ki <okashiokashi@icloud.com>
2021-06-29 16:38:27 +0900

    ADD: first,second_colorsにバリデーションを追加

--- app/models/second_color.rb
+++ app/models/second_color.rb
@@ -3,5 +3,7 @@ class SecondColor < ApplicationRecord
 
   enum part: { head: 0, body: 1, arm: 2, waist: 3, foot: 4 }
 
-  # validationを設定する
+  validates :hue, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 300 }
+  validates :saturation, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 300 }
+  validates :value, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 300 }
 end
