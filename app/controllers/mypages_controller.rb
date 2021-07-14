65c73f3e61453af695bd85abdafb7005c99a45f1
hide-ki <okashiokashi@icloud.com>
2021-07-15 06:13:08 +0900

    FIX: ソート機能の修正

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,7 +2,7 @@ class MypagesController < ApplicationController
   before_action :authenticate_user, only: [:index]
 
   def index
-    @q = current_user.posts.ransack(params[:q])
+    @q = current_user.posts.ransack(search_params)
     @posts = @q.result(distinct: true).page(params[:page]).order(created_at: :desc)
   end
 
@@ -11,5 +11,10 @@ class MypagesController < ApplicationController
   def authenticate_user
     user = User.find_by(id: params[:user_id])
     redirect_to root_path unless user && user.id == current_user.id
+  end
+
+  def search_params
+    params[:q]&.permit(:character_gender_eq, :title_cont, :sorts,
+                        :head_armor_name_cont, :body_armor_name_cont, :arm_armor_name_cont, :waist_armor_name_cont, :foot_armor_name_cont)
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,14 +2,14 @@ class StaticPagesController < ApplicationController
   skip_before_action :require_login
 
   def top
-    @q = Post.ransack(params[:q])
+    @q = Post.ransack(search_params)
     @posts = @q.result(distinct: true).page(params[:page]).order(created_at: :desc)
   end
 
   private
 
     def search_params
-      params[:q]&.permit(:character_gender, :title,
-                          :head_armor_id, :body_armor_id, :arm_armor_id, :waist_armor_id, :foot_armor_id)
+      params[:q]&.permit(:character_gender_eq, :title_cont, :sorts,
+                          :head_armor_name_cont, :body_armor_name_cont, :arm_armor_name_cont, :waist_armor_name_cont, :foot_armor_name_cont)
     end
 end
