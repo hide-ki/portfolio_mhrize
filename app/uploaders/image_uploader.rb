a44f295d088689ec21f031dec337528040f6b165
hide-ki <okashiokashi@icloud.com>
2021-07-16 08:08:39 +0900

    ADD: アップローダーのデフォルト画像を仮変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,7 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
   def default_url
-    'peach-1.jpeg'
+    'default.jpeg'
   end
 
   # def default_url(*args)
